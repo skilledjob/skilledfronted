7ee30b550698b68bdb040b24d4734b0fc224faf0
abdullahmia <abdullah149161@bsdi-bd.org>
2024-02-26 14:27:53 +0600

    fix: resolved token decode

--- app/(dashboard)/user/layout.jsx
+++ app/(dashboard)/user/layout.jsx
@@ -1,9 +1,11 @@
+import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function UserLayout({ children }) {
   const token = await cookies().get("token");
   if (token) {
+    const user = jwtDecode(token?.value);
     if (user?.role === "user") {
       return children;
     } else {
