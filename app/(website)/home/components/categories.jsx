f5675e8569f32afa947da1d0e3186b6f50f3c977
unknown <mojahidislammishat@gmail.com>
2024-03-06 02:20:50 +0600

    get category home page

--- app/(website)/home/components/categories.jsx
+++ app/(website)/home/components/categories.jsx
@@ -1,41 +1,19 @@
-"use client";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import { Navigation } from "swiper/modules";
-import Link from "next/link";
-import Image from "next/image";
-import plumber from "@/public/assets/plumber.jpg";
-import img1 from "@/public/assets/img1.jpg";
-import img2 from "@/public/assets/img2.jpg";
-import { GoChevronRight, GoChevronLeft } from "react-icons/go";
+"use client"
 
-const Categories = () => {
-  const datas = [
-    {
-      name: "Name",
-      path: img1,
-    },
-    {
-      name: "Name",
-      path: img2,
-    },
-    {
-      name: "Name",
-      path: plumber,
-    },
-    {
-      name: "Name",
-      path: img2,
-    },
-    {
-      name: "Name",
-      path: plumber,
-    },
-    {
-      name: "Name",
-      path: img1,
-    },
-  ];
+
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css/pagination";
+import "swiper/css"
+; // Import your getAllCategories function
+import { GoChevronLeft, GoChevronRight } from 'react-icons/go'; // Import your icon components
+import { getAllCategories } from '@/app/lib/jobCategories';
+
+const Categories = async () => {
+  const categoriesData = await getAllCategories();
 
   return (
     <div className="w-full mx-auto container my-6">
@@ -51,6 +29,13 @@ const Categories = () => {
             prevEl: ".swiper-Prev-Btn",
             nextEl: ".swiper-Next-Btn",
           }}
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
           breakpoints={{
             640: {
               slidesPerView: 1,
@@ -65,30 +50,31 @@ const Categories = () => {
               spaceBetween: 50,
             },
           }}
-          modules={[Navigation]}
+          modules={[Pagination, Autoplay]}
           className="mySwiper"
         >
-          {datas.map(data => (
-            <SwiperSlide key={data.path}>
-              <Link
-                href=""
-                className="flex relative h-full justify-center items-center"
-              >
-                <Image
-                  className="w-64 object-cover h-64 rounded-lg"
-                  width={256}
-                  height={256}
-                  src={data.path}
-                  alt=""
-                />
-                <div className="absolute top-0 w-full h-full left-0 px-2 py-1 font-bold bg-gradient-to-t from-black via-transparent rounded-md">
-                  <p className="absolute left-5 bottom-5 text-xs sm:text-sm md:text-xl text-white">
-                    {data.name}
-                  </p>
-                </div>
-              </Link>
-            </SwiperSlide>
-          ))}
+          {categoriesData?.length > 0 ? (
+            categoriesData?.map((category) => (
+              <SwiperSlide key={category?.id}>
+                <Link href={`/category/${category.id}`} className="flex relative h-full justify-center items-center">
+                  <Image
+                    className="w-64 object-cover h-64 rounded-lg"
+                    width={256}
+                    height={256}
+                    src={category?.image} // Assuming your category object has an 'image' property
+                    alt={category?.name} // Assuming your category object has a 'name' property
+                  />
+                  <div className="absolute top-0 w-full h-full left-0 px-2 py-1 font-bold bg-gradient-to-t from-black via-transparent rounded-md">
+                    <p className="absolute left-5 bottom-5 text-xs sm:text-sm md:text-xl text-white">
+                      {category?.name} {/* Assuming your category object has a 'name' property */}
+                    </p>
+                  </div>
+                </Link>
+              </SwiperSlide>
+            ))
+          ) : (
+            <p>No categories available</p>
+          )}
         </Swiper>
         <div className="text-white swiper-Next-Btn opacity-0 group-hover:opacity-100  h-28 bg-white/10 rounded-lg flex items-center justify-center">
           <GoChevronRight className="text-5xl cursor-pointer font-extralight" />
